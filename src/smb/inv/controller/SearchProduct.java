10992cd720a03aaa6f39acc03fb7bf24744f8bd3
jazzycrazzy <jagan.space@gmail.com>
2015-03-27 13:57:45 +0530

    Update SearchProduct.java

--- src/smb/inv/controller/SearchProduct.java
+++ src/smb/inv/controller/SearchProduct.java
@@ -1,5 +1,7 @@
 package smb.inv.controller;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import smb.inv.service.Product;
 import smb.inv.service.ProductBy;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 import smb.inv.service.FetchProductDetails;
@@ -20,6 +23,7 @@ public class SearchProduct{
 	
 	@Autowired
 	private FetchProductDetails fetchProduct;
+	private ArrayList<Product> product;
 	
 	public FetchProductDetails getFetchProduct() {
 		return fetchProduct;
@@ -35,27 +39,45 @@ public class SearchProduct{
 	}
 	
 	@RequestMapping(value="/productid/{productId}", produces="application/json")
-	public Product searchById(@PathVariable("productId") Long productID, Model model){
+	public ArrayList<Product> searchById(@PathVariable("productId") Long productID, Model model){
 		
-		return fetchProduct.fetchProductDetails(productID.toString(), ProductBy.ID);
+		try {
+			product = fetchProduct.fetchProductDetails(productID.toString(), ProductBy.ID);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return product;
 		/*model.addAttribute("product", productID);
 		model.addAttribute("searchType", "Searching product by product id ...");	
 		return "product";*/
 	}
 	
 	@RequestMapping("/productname/{productName}")
-	public Product searchByName(@PathVariable("productName") String productName, Model model){
+	public ArrayList<Product> searchByName(@PathVariable("productName") String productName, Model model){
 		
-		return fetchProduct.fetchProductDetails(productName, ProductBy.NAME);
+		try {
+			product = fetchProduct.fetchProductDetails(productName, ProductBy.NAME);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return product;
 		/*model.addAttribute("product", productName);
 		model.addAttribute("searchType", "Searching product by product name ...");
 		return "product";*/
 	}
 	
-	@RequestMapping("/model/{modelNo}")
-	public Product searchByModel(@PathVariable("modelNo") String modelNo, Model model){
+	@RequestMapping(value="/model/{modelNo}")
+	public @ResponseBody ArrayList<Product> searchByModel(@PathVariable("modelNo") String modelNo, Model model){
 		
-		return fetchProduct.fetchProductDetails(modelNo, ProductBy.MODEL);
+		try {
+			product = fetchProduct.fetchProductDetails(modelNo, ProductBy.MODEL);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return product;
 		/*model.addAttribute("product", modelNo);	
 		model.addAttribute("searchType", "Searching product by product model number ...");	
 		return "product";*/
