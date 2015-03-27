945e4c67ec1eb02dadeaabe8489efb294430dc0c
jazzycrazzy <jagan.space@gmail.com>
2015-03-27 13:56:56 +0530

    Update FetchProductDetails.java

--- src/smb/inv/service/FetchProductDetails.java
+++ src/smb/inv/service/FetchProductDetails.java
@@ -1,15 +1,15 @@
 package smb.inv.service;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class FetchProductDetails {
 	
-	@Autowired
 	private DataSource dataSource;
 	Connection conn = null;
 	
@@ -21,17 +21,33 @@ public class FetchProductDetails {
 		this.dataSource = dataSource;
 	}
 	
-	public Product fetchProductDetails(String product, ProductBy productBY) throws SQLException{
+	public ArrayList<Product> fetchProductDetails(String product, ProductBy productBY) throws SQLException{
 		
-		Product productDet = new Product();
-		
+		ArrayList<Product> productDet = new ArrayList<Product>();;
+		Statement stmt;
 		conn = dataSource.getConnection();
 		String query = "Select * from ";
 		if(productBY == ProductBy.ID)
-			query = query+"product";
+			query = query+"product NATURAL JOIN productInfo where productID="+Long.parseLong(product);
+		else if(productBY == ProductBy.MODEL)
+			query = query+"productInfo NATURAL JOIN product where modelNo=\""+product+"\"";
 		else
-			query = query+"productInfo";
+			query = query+"productInfo NATURAL JOIN product where productName=\""+product+".*\"";
+		stmt = conn.createStatement();
+		ResultSet result = stmt.executeQuery(query);
 		
+		Product p;
+		while(result.next()){
+			p = new Product();
+			p.setModelNo(result.getString("modelNo"));
+			p.setProductId(result.getLong("productID"));
+			p.setInflowDate(result.getDate("inflowDate"));
+			p.setOutflowDate(result.getDate("outflowDate"));
+			p.setDescFileLoc(result.getString("descFileLoc"));
+			p.setProductName(result.getString("productName"));
+			productDet.add(p);
+		}
+		conn.close();
 		return productDet;
 		
 	}
